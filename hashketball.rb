8f205c61149f21cd7d2798eec5416de85b2347c7
Eric Song <ericksong91@gmail.com>
2023-02-03 17:22:38 -0500

    done with assignment

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 # Write your code below game_hash
 def game_hash
   {
@@ -126,4 +128,116 @@ def game_hash
   }
 end
 
-# Write code here
+def num_points_scored(name)
+  game_hash[:home][:players].find{|player| 
+    if player[:player_name] == name then
+      return player[:points]
+    end
+  }
+
+  game_hash[:away][:players].find{|player| 
+    if player[:player_name] == name then
+      return player[:points]
+    end
+  }
+end
+
+def shoe_size(name)
+  game_hash[:home][:players].find{|player| 
+    if player[:player_name] == name then
+      return player[:shoe]
+    end
+  }
+
+  game_hash[:away][:players].find{|player| 
+    if player[:player_name] == name then
+      return player[:shoe]
+    end
+  }
+end
+
+def team_colors(name)
+  game_hash[:home].find{|team|
+    if team[1] == name then
+      return game_hash[:home][:colors]
+    end
+  }
+
+  game_hash[:away].find{|team|
+    if team[1] == name then
+      return game_hash[:away][:colors]
+    end
+  }
+end
+
+def team_names
+teams = []
+
+teams << game_hash[:home][:team_name]
+
+teams << game_hash[:away][:team_name]
+
+teams
+
+end
+
+def player_numbers(name)
+  jersey_numbers = []
+
+  game_hash.find{|team| 
+    if team[1][:team_name] == name then
+      team[1][:players].each{|player| 
+        jersey_numbers << player[:number]
+      }
+    end
+  }
+  jersey_numbers
+end
+
+def player_stats(name)
+
+  game_hash[:home][:players].find{|player| 
+    if player[:player_name] == name then
+      return player
+    end
+  }
+
+  game_hash[:away][:players].find{|player| 
+    if player[:player_name] == name then
+      return player
+    end
+  }
+
+end
+
+def big_shoe_rebounds
+  #Look at list of players of both teams
+  #Find maximum of that list
+  #Find player with that shoe size
+  #return that player's rebounds
+
+  shoes = []
+
+  game_hash[:home][:players].map {|player|
+    shoes << player[:shoe]
+  }
+
+  game_hash[:away][:players].map {|player|
+    shoes << player[:shoe]
+  }
+
+  max_value = shoes.max
+
+  game_hash[:home][:players].find{|player|
+    if player[:shoe] == max_value then
+      return player[:rebounds]
+    end
+  }
+
+  game_hash[:away][:players].find{|player|
+    if player[:shoe] == max_value then
+      return player[:rebounds]
+    end
+  }
+
+end
